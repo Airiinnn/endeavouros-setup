4f8ca83d66f7685f2f2a85724ecdacc81e65801a
alex <alex.liswandy@hotmail.com>
2024-06-17 23:40:15 +0800

    Added LazyVim

---
+++ .config/nvim.bak/init.lua
@@ -0,0 +1,12 @@
+require("keymaps")
+require("settings")
+require("plugins")
+require("nvim-cmp")
+require("autopairs")
+require("treesitter")
+
+vim.cmd("colorscheme catppuccin")
+
+require("nvim-tree").setup()
+require("lualine").setup()
+
